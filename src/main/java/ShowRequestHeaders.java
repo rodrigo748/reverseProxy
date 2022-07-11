958fedb436b40cd72bcdc2f4d6432f161f918ee3
rodrigo748 <62340693+rodrigo748@users.noreply.github.com>
2022-07-11 17:41:33 -0400

    Update ShowRequestHeaders.java

--- src/main/java/ShowRequestHeaders.java
+++ src/main/java/ShowRequestHeaders.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ShowRequestHeaders")
+// @WebServlet("/ShowRequestHeaders")
 
 public class ShowRequestHeaders extends HttpServlet {
    
