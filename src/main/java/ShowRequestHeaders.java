093e61e85f503a0e897ea3f0207578ae79058551
rodrigo748 <62340693+rodrigo748@users.noreply.github.com>
2022-07-11 17:00:14 -0400

    Update ShowRequestHeaders.java

--- src/main/java/ShowRequestHeaders.java
+++ src/main/java/ShowRequestHeaders.java
@@ -1,40 +1,56 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.*;
-import org.jfree.chart.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* @WebServlet("/RequestHeadersServlet") */
 
 public class ShowRequestHeaders extends HttpServlet {
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    String title = "Servlet Example: Showing Request Headers";
-    out.println(ServletUtilities.headWithTitle(title) +
-                "<BODY BGCOLOR=\"#FDF5E6\">\n" +
-                "<H1 ALIGN=CENTER>" + title + "</H1>\n" +
-                "<B>Request Method: </B>" +
-                request.getMethod() + "<BR>\n" +
-                "<B>Request URI: </B>" +
-                request.getRequestURI() + "<BR>\n" +
-                "<B>Request Protocol: </B>" +
-                request.getProtocol() + "<BR><BR>\n" +
-                "<TABLE BORDER=1 ALIGN=CENTER>\n" +
-                "<TR BGCOLOR=\"#FFAD00\">\n" +
-                "<TH>Header Name<TH>Header Value");
-    Enumeration headerNames = request.getHeaderNames();
-    while(headerNames.hasMoreElements()) {
-      String headerName = (String)headerNames.nextElement();
-      out.println("<TR><TD>" + headerName);
-      out.println("    <TD>" + request.getHeader(headerName));
-    }
-    out.println("</TABLE>\n</BODY></HTML>");
-  }
+   
+ protected void doGet(
+   HttpServletRequest request,
+   HttpServletResponse response)
+   throws ServletException, IOException
+ {
+  
+  doPost(request, response);
+    
+ }
 
-  public void doPost(HttpServletRequest request,
-                     HttpServletResponse response)
-      throws ServletException, IOException  {
-    doGet(request, response);
+ 
+ protected void doPost(
+   HttpServletRequest request, 
+   HttpServletResponse response) 
+   throws ServletException, IOException 
+ {
+  Enumeration<String> headers = request.getHeaderNames();
+  PrintWriter out = response.getWriter();
+  response.setContentType("text/html");
+  out.print("<html>");
+  out.print("<body>");
+  out.print("<h1>The Request Headers are as follows : </h1>");
+  out.print("<table border='1'>");  
+  
+  while(headers.hasMoreElements())
+  {
+   String header = headers.nextElement();
+   String headerValue = request.getHeader(header);
+   out.print("<tr>");
+   out.print("<td>");
+   out.print(header);
+   out.print("</td>");
+   out.print("<td>");
+   out.print(headerValue);   
+   out.print("</td>");
+   out.print("</tr>");
   }
+  out.print("</table>");
+  out.print("</body>");  
+  out.print("</html>");
+
+ }
 }
